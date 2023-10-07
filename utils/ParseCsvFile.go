722414e787e51dcd14c219599856a55ab8b82239
Delnegend <kiennguyen19323@gmail.com>
2023-10-08 01:00:07 +0700

    refactor(ParseCsvFile): :recycle: remove unnecessary comments

--- utils/ParseCsvFile.go
+++ utils/ParseCsvFile.go
@@ -42,35 +42,18 @@ func ParseCsvFile(filePath string) (map[string]Country, error) {
 	// Iterate through the records, starting at row 2
 	for i, row := range records {
 		if i == 0 {
-			// Skip the header row
 			continue
 		}
 
-		// Parse the date
+		// Parse the data
 		date := row[0]
-
-		// Parse the new cases
 		newCases := parseInt(row[4])
-
-		// Parse the new deaths
 		newDeaths := parseInt(row[6])
-
-		// Parse the cumulative cases
 		cumulativeCases := parseInt(row[5])
-
-		// Parse the cumulative deaths
 		cumulativeDeaths := parseInt(row[7])
-
-		// Parse the country code
 		countryCode := row[1]
-
-		// Parse the country name
 		countryName := row[2]
-
-		// Parse the WHO region
 		whoRegion := row[3]
-
-		// Create a new daily report
 		report := DailyReport{
 			Date:             date,
 			NewCases:         newCases,
